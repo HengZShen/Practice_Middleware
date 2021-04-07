65876cda278dc32dcb5605681d8ac21ef9ecce1a
HengZShen <r06621112@gmail.com>
2021-04-07 21:08:16 +0800

    refactor: order of route

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,13 @@ const express = require('express')
 
 const router = express.Router()
 
-
 const home = require('./modules/home')
 
 const todos = require('./modules/todos')
 
-const midFunction = require('./modules/middlewareFunction')
 
-
-
-router.use('/', home)
 router.use('/todos', todos)
-router.use('/test', midFunction)
-
+router.use('/', home)
 
 
 
